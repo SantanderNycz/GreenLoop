96d2d3b4f08a01ccebefa4d206f5c18f938317dd
Leo Santander <santandernycz.ls@gmail.com>
2025-10-14 19:50:46 +0100

    atualização do logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,7 @@
+import logo from "../assets/logo.png";
+
 const Logo = ({ className = "w-12 h-12" }: { className?: string }) => {
-  return <img src="./src/assets/logo.png" alt="Logo" className={className} />;
+  return <img src={logo} alt="Logo" className={className} />;
 };
 
 export default Logo;
